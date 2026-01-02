78d2f1a5320a03389a9ed4f184911441b1c73636
bereke1t2 <bereketaschalew1212@gmail.com>
2026-01-02 13:18:07 +0000

    started reader future

---
+++ mobile/lib/features/reader/presentation/pages/downloaded_book_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReaderPage extends StatelessWidget {
+  const ReaderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
